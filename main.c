ac6a8919510cebed4ae4fe047913e1c98d1d5c0d
Gregory Huras <30447480+gr8gregory@users.noreply.github.com>
2020-03-02 10:54:13 -0500

    Add files via upload

--- main.c
+++ main.c
@@ -9,18 +9,50 @@
 #include "stm32l476xx.h"
 #include "SysClock.h"
 #include "GPIO.h"
+#include "lcd.h"
 #include "Servo.h"
 
+float TimeFromReading(uint16_t reading){
+	
+	return reading * 0.00044 + 0.6;
+}
 
-int
-main(void) {
+uint16_t MilVoltsRead(uint16_t reading){
+	
+	return (uint16_t)(3300 * reading / 0xFFF);
+}
+
+// 180/OxFFF - This will 
+uint16_t AngleConversion(uint16_t reading){
+	
+	return(int16_t)(180 * reading / 0xFFF);
+}
+
+int main(void) {
+
+
+
 	
 	System_Clock_Init();
+	// Initalize the GPIO
 	GPIO_CLOCK_ENABLE();
-	GPIO_Init();
+	GPIOE_Init();
+	
+	// LCD Init
+	LCDinit();
+	
+	// Intialize the Timer
 	TIMER_CLOCK_ENABLE();
 	Timer_Init();
 	
+	// Initalize the ADC
+	ADC_CLK();
+	ADC_init();
+	
+	
+	
+	
+	/*
 	int i;
 	int brightness = 500;
 	int stepSize = 1;
@@ -38,5 +70,25 @@ main(void) {
 	}
 	
 	// uint32_t count = 0;
+	*/
+	
+	
+	uint16_t ADCReading, MilVolts, Angle;
+	while(1){
+		
+		//ADCvalue is the 16 bit value outputed from the ADC register
+		ADCReading = triggerADCConv();
+		
+		
+		// Convertes the ADC value (0 - 0xFFF) to a voltage
+		MilVolts = MilVoltsRead(ADCReading);
+		
+		// Converts the ADC Value into an Angle
+		Angle = AngleConversion(ADCReading);
+		
+		//Print the following Values to the LCD
+		LCDprintf("ADC:%d, V:%d, A:%d",ADCReading, MilVolts, Angle);
+		Delay_ms(100);
+	}
 		
 }
